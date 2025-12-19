c74dd2f5617fbf66918307f0d32e484fa8d47193
Giancarlo <g.salomone@live.com>
2025-12-18 22:49:26 -0300

    Semi Final Feat: Implementado Keycloack + Gateway

--- gateway/HospitalGateway/Program.cs
+++ gateway/HospitalGateway/Program.cs
@@ -1,12 +1,43 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+var keycloakAuthority = builder.Configuration["Keycloak:Authority"];
+var keycloakAudience = builder.Configuration["Keycloak:Audience"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = keycloakAuthority;
+        options.Audience = keycloakAudience;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = false, // Desabilitar validação do issuer por enquanto
+            ValidateAudience = true,
+            ValidAudiences = new[] { keycloakAudience },
+            ValidateLifetime = true
+        };
+    });
+
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 
-// Swagger para o Gateway também
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+await app.UseOcelot();
 
 app.UseSwagger();
 app.UseSwaggerUI();
